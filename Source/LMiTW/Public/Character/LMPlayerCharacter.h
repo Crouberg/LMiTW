990bb1d47617973ce3f338066c1605175e840270
Chargos81 <Chargos81@gmail.com>
2021-04-19 18:26:59 +0300

    Create basic cpp classes

---
+++ Source/LMiTW/Public/Character/LMPlayerCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/LMBaseCharacter.h"
+#include "LMPlayerCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LMITW_API ALMPlayerCharacter : public ALMBaseCharacter
+{
+	GENERATED_BODY()
+	
+};
